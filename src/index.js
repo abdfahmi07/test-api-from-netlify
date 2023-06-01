2d025bd1ed9393614560d8aed339e1f60ea2da8b
Abdulloh <abdullohfahmi45@gmail.com>
2023-06-01 14:31:16 +0700

    update url

--- src/index.js
+++ src/index.js
@@ -1,37 +1,27 @@
-const login = async (payloads) => {
-  console.log(payloads);
+const login = async () => {
+  //   console.log(payloads);
 
-  const response = await fetch(
-    "http://ec2-13-229-98-157.ap-southeast-1.compute.amazonaws.com:8000/users/login",
-    {
-      method: "POST",
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      referrerPolicy: "no-referrer",
-      body: JSON.stringify(payloads),
-    }
-  );
-  console.log(response);
+  const response = await fetch("https://52.77.30.160:8000/items");
+  // console.log(response);
   const result = await response.json();
 
-  console.log(result);
+  console.log("res", result);
 };
 
-const form = document.getElementById("form");
+// const form = document.getElementById("form");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
 
-  const payloads = {
-    email,
-    password,
-  };
+//   const payloads = {
+//     email,
+//     password,
+//   };
 
-  login(payloads);
-});
+//   login(payloads);
+// });
+
+login();
